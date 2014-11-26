13391f286637391f4b6aea3bf8ff94df4edf8fab
Jack Matthews <jackmatt2@gmail.com>
2014-11-26 22:29:13 +1100

    add jsmockito library

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-jsmockito'
+  name: 'ember-cli-jsmockito',
+  afterInstall: function() {
+	  return this.addBowerPackageToProject('jshamcrest');
+  }
 };
