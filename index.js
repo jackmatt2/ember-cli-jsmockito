546983239a5a627aa028ff2442eb5bdb63feb5aa
Jack Matthews <j.matthews@patrick.com.au>
2014-11-27 12:41:56 +1100

    inject code into consuming test-head

--- index.js
+++ index.js
@@ -4,5 +4,23 @@ module.exports = {
   name: 'ember-cli-jsmockito',
   afterInstall: function() {
 	  return this.addBowerPackageToProject('jshamcrest');
+  },
+  contentFor: function(type, config) {
+	  
+	  if(type === 'test-body') {
+		  var injectJsMockito = '\
+		    <script src="ember-cli-jsmockito/assets/tests/jshamcrest.js"></script>\
+		    <script src="ember-cli-jsmockito/assets/tests/jsmockito.js"></script>\
+		    <script language="javascript">\
+			  $( document ).ready(function() {\
+			      JsMockito.Integration.QUnit();\
+			  });\
+		    </script>\
+		  ';
+		    
+		  return injectJsMockito;
+	  }
+	  
+	  return '';
   }
 };
