637894d51fc06243ff6e23e720c9dca1440ffb55
Jack Matthews <jackmatt2@gmail.com>
2014-11-30 00:50:16 +1100

    jsmockito qunit hook issue

--- index.js
+++ index.js
@@ -5,20 +5,28 @@ module.exports = {
   afterInstall: function() {
 	  return this.addBowerPackageToProject('jshamcrest');
   },
+  included: function(app) {
+	  this._super.included(app);
+	  
+	  app.import('vendor/jsmockito.js', {
+		  type: 'test'
+	  });
+	  app.import('vendor/jshamcrest.js', {
+		  type: 'test'
+	  });
+  },
   contentFor: function(type, config) {
 	  
-	  if(type === 'test-body') {
-		  var injectJsMockito = '\
-		    <script src="ember-cli-jsmockito/assets/tests/jshamcrest.js"></script>\
-		    <script src="ember-cli-jsmockito/assets/tests/jsmockito.js"></script>\
-		    <script language="javascript">\
-			  $( document ).ready(function() {\
-			      JsMockito.Integration.QUnit();\
-			  });\
-		    </script>\
+	  if(type === 'test-body') { 
+		  var setupJsMockito = '\
+			<script language="javascript">\n\
+			  $( document ).ready(function() {\n\
+			      JsMockito.Integration.QUnit();\n\
+			  });\n\
+		    </script>\n\
 		  ';
 		    
-		  return injectJsMockito;
+		  return setupJsMockito;
 	  }
 	  
 	  return '';
